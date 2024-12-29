b606d371792a9084914c1b5e33a352ab88541c23
yuvarajmadineni <yuvarajuv49@gmail.com>
2024-12-29 22:47:49 +0530

    add avante plugin for ai code generation

--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,7 @@ require "nvchad.options"
 local o = vim.o
 o.foldmethod = "expr"
 o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldlevelstart = 99
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
